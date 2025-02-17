e9e848a302b9900bf6f3a6391ad167eac0f9d1b8
Paixpascy <Paixpascy24@gmail.com>
2025-02-17 14:10:32 +0200

    added home and source files

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,13 @@ import './App.css'
 import Header from './assets/Header'
 import Footer from './assets/Footer'
 import Content from './assets/Content';
+import Home from './assets/Home';
 function App() {
   return(
     <>
      <Header/>
+     <Home/>
+     <Footer/>
     </>
   );
   
